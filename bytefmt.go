eeb8b0a09370a5f5dffc035293e3ca03818a56a2
Danil Kutkevich <danil@kutkevich.org>
2021-03-02 13:06:20 +0300

    minor

--- bytefmt.go
+++ bytefmt.go
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package bytefmt provides Bytes type whats implements fmt.Formatter interface
-
+// Package bytefmt provides Bytes type whats implements fmt.Formatter interface.
 package bytefmt
 
 import (
